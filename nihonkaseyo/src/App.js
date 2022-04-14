545264b0e5377b046f48b5f2ebb6b9eea3366314
Ekaraja <75461147+Ekaraja@users.noreply.github.com>
2022-04-14 14:57:41 +0200

    set text to IPA + IPA to japanese

---
+++ nihonkaseyo/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import ToIPA from './components/ToIPA';
+import ToJapanize from './components/ToJapanize';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+< ToIPA/>
+< ToJapanize/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
